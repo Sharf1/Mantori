0dbcffa1f46496b6bdcdabf45a2c549036b72b8f
Sharf <aellen.virwolt@yandex.ru>
2022-02-14 16:26:29 +0300

    commit

--- src/main/java/net/mantori/sounds/ModSounds.java
+++ src/main/java/net/mantori/sounds/ModSounds.java
@@ -6,7 +6,18 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModSounds {
-    public static SoundEvent WING_FLAP = registerSoundEvent("wing_flap");
+    public static SoundEvent WING_FLAP = registerSoundEvent("aphid.wing_flap");
+    public static SoundEvent AMBIENT = registerSoundEvent("aphid.ambient");
+    public static SoundEvent ANGRY = registerSoundEvent("aphid.angry");
+    public static SoundEvent BREATHE = registerSoundEvent("aphid.breathe");
+    public static SoundEvent DEATH = registerSoundEvent("aphid.death");
+    public static SoundEvent EAT = registerSoundEvent("aphid.eat");
+    public static SoundEvent GALLOP = registerSoundEvent("aphid.gallop");
+    public static SoundEvent HURT = registerSoundEvent("aphid.hurt");
+    public static SoundEvent JUMP = registerSoundEvent("aphid.jump");
+    public static SoundEvent LAND = registerSoundEvent("aphid.land");
+    public static SoundEvent FOOTSTEPS = registerSoundEvent("aphid.footstep");
+    public static SoundEvent FOOTSTEPS_WOOD = registerSoundEvent("aphid.footstep.wood");
 
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = new Identifier(Mantori.MOD_ID, name);
